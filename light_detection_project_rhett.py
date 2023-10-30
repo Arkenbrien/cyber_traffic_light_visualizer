3f62ba2c55c85bad68ecf2ab255828833bf54651
tm769613@ohio.edu <tm769613@ohio.edu>
2023-10-30 17:16:41 -0400

    live video plot

--- light_detection_project_rhett.py
+++ light_detection_project_rhett.py
@@ -18,81 +18,56 @@
 # -*- coding: utf-8 -*-
 """Module for example of listener."""
 
-from cyber_py import cyber
+from cyber_py3 import cyber
 from modules.drivers.proto.sensor_image_pb2 import CompressedImage
 from traffic_light_custom import TrafficLightDetection
 import time
 import numpy as np
-
 import cv2
 
-class traffic_img_listener:
-    def __init__(self):
-        self.camera_topic = "/apollo/sensor/camera/front_6mm/image/compressed"
-        self.traffic_light_topic     = "/apollo/sensor/camera/front_6mm/image/compressed"
 
+def image_callback(msg_camera):
+    print("py:reader callback msg->:")
+    # print(data)
+    # msg_camera = CompressedImage()
+    # print(msg_camera.data)
+    # start_time = time.time()
+    decoded_image = cv2.imdecode(np.frombuffer(msg_camera.data, np.uint8), cv2.IMREAD_COLOR)
+    rgb_image = cv2.cvtColor(decoded_image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+    # print(time.time()-start_time)
+    # cv2.imwrite('output_image.jpg', image)
 
-    def image_callback(self, msg_camera):
-        print("py:reader callback msg->:")
-        # print(data)
-        # msg_camera = CompressedImage()
-        # print(msg_camera.data)
-        # start_time = time.time()
-        # while 1:
-        decoded_image = cv2.imdecode(np.frombuffer(msg_camera.data, np.uint8), cv2.IMREAD_COLOR)
-        rgb_image = cv2.cvtColor(decoded_image, cv2.COLOR_BGR2RGB)
-        self.image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
-        # print(time.time()-start_time)
-        # cv2.imwrite('output_image.jpg', image)
+    # cv2.imshow('Image', image)
+    # print(type(msg_camera))
+    # print(dir(msg_camera))
 
-        # cv2.imshow('Image', self.image)
-        # cv2.waitKey(1)
-        # print(type(msg_camera))
-        # print(dir(msg_camera))
+    # print('SUCESS! (?????)')
+    # time.sleep(10000)
+    # msg_camera.ParseFromString(str(msg_camera))
+    # print(msg_camera)
+    # print("=" * 80)
 
-        # print('SUCESS! (?????)')
-        # time.sleep(10000)
-        # msg_camera.ParseFromString(str(msg_camera))
-        # print(msg_camera)
-        # print("=" * 80)
+def tl_callback(traffic_light):
+    # print(type(traffic_light))
+    # print(dir(traffic_light))
+    print(traffic_light.contain_lights)
+    time.sleep(10000)
 
-    def tl_callback(self, tl_msg):
-        # print(type(traffic_light))
-        # print(dir(traffic_light))
-        # print(tl_msg.contain_lights)
-        time.sleep(10000)
+def traffic_light_listener(traffic_light_topic):
+    tl_node = cyber.Node("listener")
+    tl_node.create_reader(traffic_light_topic, TrafficLightDetection, tl_callback)
+    tl_node.spin()
 
-    # def traffic_light_listener(self):
-    #     # tl_node = cyber.Node("listener")
-    #     tl_node.create_reader(self.traffic_light_topic, TrafficLightDetection, self.tl_callback)
-    #     # tl_node.spin()
-
-    # def camera_listener(self):
-    #     # camera_node = cyber.Node("listener")
-    #     camera_node.create_reader(self.camera_topic, CompressedImage, self.image_callback)
-    #     camera_node.spin()
+def camera_listener(camera_topic):
+    camera_node = cyber.Node("listener")
+    camera_node.create_reader(camera_topic, CompressedImage, image_callback)
+    camera_node.spin()
 
 if __name__ == '__main__':
-    traffic_light_handler = traffic_img_listener()
-    
-
     cyber.init()
-    
-    tl_node = cyber.Node("listener")
-
-    # tl_node.create_reader(traffic_light_handler.traffic_light_topic, TrafficLightDetection, traffic_light_handler.tl_callback)
-    tl_node.create_reader(traffic_light_handler.camera_topic, CompressedImage, traffic_light_handler.image_callback)
-
-    time.sleep(5)
-
-    while 1:
-        try:
-            cv2.imshow('Image', traffic_light_handler.image)
-            cv2.waitKey(1)
-        except:
-            continue
-    
-
+    traffic_light_listener("/apollo/perception/traffic_light")
+    camera_listener("/apollo/sensor/camera/front_6mm/image/compressed")
     cyber.shutdown()
 
 
@@ -131,4 +106,3 @@ if __name__ == '__main__':
 # '_cached_byte_size', '_cached_byte_size_dirty', '_decoders_by_tag', '_extensions_by_name', 
 # '_extensions_by_number', '_fields', '_is_present_in_parent', '_listener', '_listener_for_children', 
 # '_oneofs', '_unknown_fields', 'data', 'format', 'frame_id', 'frame_type', 'header', 'measurement_time']
-
