ea70fda0f11b90ac71417f7c5571c0d42be9ff1f
Autobuntu <rh166617@ohio.edu>
2023-11-01 13:39:58 -0400

    Fixed video export bugs

--- light_detection_project_p3.py
+++ light_detection_project_p3.py
@@ -2,9 +2,8 @@
 
 # ****************************************************************************
 # Travis Moleski, Rhett Huston
+# Updated 11/01/2023
 # ****************************************************************************
-
-
 
 from cyber_py import cyber
 from modules.drivers.proto.sensor_image_pb2 import CompressedImage
@@ -16,33 +15,43 @@ import cv2
 class traffic_img_listener:
     
     def __init__(self):
+        
         self.camera_topic_25mm      = "/apollo/sensor/camera/front_25mm/image/compressed"
         self.camera_topic_06mm      = "/apollo/sensor/camera/front_6mm/image/compressed"
         self.traffic_light_topic    = "/apollo/perception/traffic_light"
         self.is_first_frame            = True
 
     def image_callback_6mm(self, msg_camera):
+        
         decoded_image = cv2.imdecode(np.frombuffer(msg_camera.data, np.uint8), cv2.IMREAD_COLOR)
         rgb_image = cv2.cvtColor(decoded_image, cv2.COLOR_BGR2RGB)
         self.image_6mm = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR) 
         self.image_6mm_ts = msg_camera.header.timestamp_sec
-        if self.is_first_frame:
+        
+        if self.is_first_frame == True:
+            
             self.init_ts = msg_camera.header.timestamp_sec
             self.is_first_frame = False
 
     def image_callback_25mm(self, msg_camera):
+        
         decoded_image = cv2.imdecode(np.frombuffer(msg_camera.data, np.uint8), cv2.IMREAD_COLOR)
         rgb_image = cv2.cvtColor(decoded_image, cv2.COLOR_BGR2RGB)
         self.image_25mm = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
         self.image_25mm_ts = msg_camera.header.timestamp_sec
-        if self.is_first_frame:
+        
+        
+        if self.is_first_frame == True:
+            
             self.init_ts = msg_camera.header.timestamp_sec
             self.is_first_frame = False
 
     def get_init_ts(self, msg_camera):
+        
         init_ts = 1
         
     def color_check(self, colorState):
+        
         if colorState == 3:
             color = (0,255,0)
             cString = "green"
@@ -58,23 +67,28 @@ class traffic_img_listener:
         return cString, color
         
     def tl_info_callback(self, tl_msg):
+        
         # print(tl_msg)
         self.tl_info = tl_msg
         
     def cropbox_printer(self, roi, bColor):
+        
         cv2.rectangle(img,(roi.x, roi.y),(roi.x+roi.width, roi.y+roi.width),bColor, 5)
     
     def box_printer(self, roi, bColor):
+        
         for b in roi:
             cv2.rectangle(img,(b.x, b.y),(b.x+b.width, b.y+b.width), bColor, 2)
             
     def debug_box_printer(self, roi, bColor):
+        
         for b in roi:
             cv2.rectangle(img,(b.x, b.y),(b.x+b.width, b.y+b.width),bColor, 2)
             if hasattr(b, 'selected') and b.selected==True:
                 cv2.rectangle(img,(b.x, b.y),(b.x+b.width, b.y+b.width),(255,255,255), 2)
                 
     def text_handler(self, cString, dString, color):
+        
         corg = (50, 50) 
         dorg = (50, 90) 
         font = cv2.FONT_HERSHEY_SIMPLEX 
@@ -95,33 +109,41 @@ class cv2_video_writer:
         self.video = cv2.VideoWriter(self.output_name, self.fourcc, 120, (1360,768))
         self.append_ready = True
         self.init_ts = init_ts
+        
         self.first_frame = True
+        self.exported = False
         
     def add_frame(self, img, ts):
         
-        print(ts != self.init_ts, self.first_frame, self.append_ready)
+        # print(ts != self.init_ts, self.first_frame, self.append_ready)
+        # print(self.append_ready, self.first_frame, self.exported)
+        # print(self.init_ts, ts)
+        self.ts = ts
+        # print('{0:.30f}'.format(self.init_ts), '{0:.30f}'.format(self.ts))
         
         if self.first_frame == True:
             self.video.write(img)
-            print('IMAGE APPEHNDED') 
             self.first_frame = False
             
-        if ts!= self.init_ts and not self.first_frame and self.append_ready:
+        elif self.ts!= self.init_ts and self.first_frame == False and self.append_ready == True:
             self.video.write(img)
-            print('IMAGE APPEHNDED')
             
-        if ts == self.init_ts and not self.first_frame:
-            self.video.write(img)
-            print('IMAGE APPEHNDED')
-            print('LAST FRAME REACHED')
+        elif self.ts == self.init_ts and self.first_frame == False:
+
             self.append_ready = False
-            self.export_video()
+            
+            if self.exported == False:
+                self.video.write(img)
+                self.export_video()
+                self.exported = True
+            
+        # else:
+            
+        #     print('D')
         
     def export_video(self):
         self.video.release()
-        print('='*80)
         print('VIDEO RELEASED') 
-        print('='*80)
         
 if __name__ == '__main__':
     
@@ -139,19 +161,20 @@ if __name__ == '__main__':
     
     time.sleep(1)
     
-    out_video = cv2_video_writer(str(time.time()), traffic_light_handler.init_ts)
-    
+    out_video = cv2_video_writer(str(time.time()), traffic_light_handler.image_6mm_ts)
+
     while 1:
-                        
+        
+        ts = traffic_light_handler.image_6mm_ts
+        
         if traffic_light_handler.tl_info.contain_lights:
             
             if traffic_light_handler.tl_info.camera_id == 2:
                img = traffic_light_handler.image_6mm
-               ts = traffic_light_handler.image_6mm_ts
-            elif traffic_light_handler.tl_info.camera_id == 0:
-               img = traffic_light_handler.image_25mm
-               ts = traffic_light_handler.image_25mm_ts
                
+            elif traffic_light_handler.tl_info.camera_id == 0:
+               img = traffic_light_handler.image_25mm  
+                            
             cString, color = traffic_light_handler.color_check(traffic_light_handler.tl_info.traffic_light[0].color)
                         
             traffic_light_handler.cropbox_printer(traffic_light_handler.tl_info.traffic_light_debug.cropbox, color)
@@ -179,9 +202,6 @@ if __name__ == '__main__':
             out_video.add_frame(img, traffic_light_handler.image_6mm_ts)
             cv2.imshow('Image', img)
             cv2.waitKey(1)
-            
-        
-            
 
     cyber.shutdown()
 
